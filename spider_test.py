b5236aad5811428b80376c1254229dcb6bd4c571
DeronLee <akagilnc@gmail.com>
2016-11-07 21:24:41 +0800

    add spider_test

---
+++ spider_test.py
@@ -0,0 +1,22 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        'http://quotes.toscrape.com/tag/humor/',
+    ]
+    scrapy.Spider.AUTOTHROTTLE_ENABLED = True
+    scrapy.Spider.AUTOTHROTTLE_DEBUG = True
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.xpath('span/small/text()').extract_first(),
+            }
+
+        next_page = response.css('li.next a::attr("href")').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
